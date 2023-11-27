6b0d254f56f02ea5ac0ad8353396957ab76acada
Mukesh Mhanta <mukesh.mhanta@websynergies.com>
2023-11-27 18:08:48 +0530

    side bar changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,7 +15,7 @@ function Sidebar() {
             <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark slide-in-left">
                 <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                     <Link to="/" className="d-flex align-items-center pb-1 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span className="fs-5 d-none d-sm-inline ff-jost fw-600">Yagnik</span>
+                        <span className="fs-5 d-none d-sm-inline ff-jost fw-600">Mukesh</span>
                     </Link>
                     <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                         <li className="nav-item">
